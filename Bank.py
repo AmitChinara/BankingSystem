87d64548f4cefd8737470305eb8b7f40eff8d405
Amit Chinara <amitchinara@gmail.com>
2024-03-17 02:01:17 +0530

    Added Function for Account number generation

--- Bank.py
+++ Bank.py
@@ -1,4 +1,11 @@
+import random
+
+
 class Bank:
 
     def __init__(self):
         pass
+
+    # Generates a six-digit account number for a new bank account.
+    def generateAccountNumber(self):
+        return str(random.randint(100000, 999999))
