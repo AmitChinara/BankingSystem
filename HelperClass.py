48d7b2fe94275efce363b3434fdb0d50b3a7cd49
Amit Chinara <amitchinara@gmail.com>
2024-03-20 21:54:19 +0530

    Made changes in HelperClass.py for print()

--- HelperClass.py
+++ HelperClass.py
@@ -4,18 +4,18 @@ class Helper:
         pass
 
     def displayMenu(self):
-        print("++---------------|| WELCOME TO SONAL BANK ||-------------------++")
-        print("||                   1. Account Creation                       ||")
-        print("||                   2. Deposit                                ||")
-        print("||                   3. Withdrawal                             ||")
-        print("||                   4. Balance Inquiry                        ||")
-        print("||                   5. Close Account                          ||")
-        print("||                   6. Exit Application                       ||")
-        print("++-------------------------------------------------------------++")
+        print("++---------------|| WELCOME TO SONAL BANK ||-------------------++"
+              "||                   1. Account Creation                       ||"
+              "||                   2. Deposit                                ||"
+              "||                   3. Withdrawal                             ||"
+              "||                   4. Balance Inquiry                        ||"
+              "||                   5. Close Account                          ||"
+              "||                   6. Exit Application                       ||"
+              "++-------------------------------------------------------------++")
 
     def displayDetails(self, account_number, name, balance):
-        print("+=================================================+")
-        print(f"  Account Number: {account_number}")
-        print(f"  Account Holder Name: {name}")
-        print(f"  Available Balance: ${balance:.2f}")
-        print("+=================================================+")
+        print("+=================================================+"
+              f"  Account Number: {account_number}"
+              f"  Account Holder Name: {name}"
+              f"  Available Balance: ${balance:.2f}"
+              "+=================================================+")
